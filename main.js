8bc8a3640d9fca5c337d6919b10096887b4283dd
User <user@example.com>
2025-11-07 10:08:44 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,111 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const base_1 = require("@companion-module/base");
+class LinuxShowPlayerInstance extends base_1.InstanceBase {
+    constructor() {
+        super(...arguments);
+        this.config = {
+            host: '127.0.0.1',
+            port: 8000
+        };
+    }
+    async init(config) {
+        this.config = config;
+        this.updateStatus(base_1.InstanceStatus.Ok);
+        this.updateActions();
+        this.updateFeedbacks();
+    }
+    async configUpdated(config) {
+        this.config = config;
+        this.updateStatus(base_1.InstanceStatus.Ok);
+    }
+    async destroy() {
+        // Cleanup if needed
+    }
+    getConfigFields() {
+        return [
+            {
+                type: 'textinput',
+                id: 'host',
+                label: 'Target IP',
+                width: 8,
+                default: '127.0.0.1'
+            },
+            {
+                type: 'number',
+                id: 'port',
+                label: 'Target Port',
+                width: 4,
+                default: 8000,
+                min: 1,
+                max: 65535
+            }
+        ];
+    }
+    updateActions() {
+        this.setActionDefinitions({
+            start_cue: {
+                name: 'Start Cue',
+                options: [
+                    {
+                        type: 'textinput',
+                        id: 'cue_id',
+                        label: 'Cue ID',
+                        default: '1'
+                    }
+                ],
+                callback: async (action) => {
+                    this.oscSend(this.config.host, this.config.port, `/cue/${action.options.cue_id}/start`, []);
+                }
+            },
+            stop_cue: {
+                name: 'Stop Cue',
+                options: [
+                    {
+                        type: 'textinput',
+                        id: 'cue_id',
+                        label: 'Cue ID',
+                        default: '1'
+                    }
+                ],
+                callback: async (action) => {
+                    this.oscSend(this.config.host, this.config.port, `/cue/${action.options.cue_id}/stop`, []);
+                }
+            },
+            pause_cue: {
+                name: 'Pause Cue',
+                options: [
+                    {
+                        type: 'textinput',
+                        id: 'cue_id',
+                        label: 'Cue ID',
+                        default: '1'
+                    }
+                ],
+                callback: async (action) => {
+                    this.oscSend(this.config.host, this.config.port, `/cue/${action.options.cue_id}/pause`, []);
+                }
+            },
+            resume_cue: {
+                name: 'Resume Cue',
+                options: [
+                    {
+                        type: 'textinput',
+                        id: 'cue_id',
+                        label: 'Cue ID',
+                        default: '1'
+                    }
+                ],
+                callback: async (action) => {
+                    this.oscSend(this.config.host, this.config.port, `/cue/${action.options.cue_id}/resume`, []);
+                }
+            }
+        });
+    }
+    updateFeedbacks() {
+        // No feedbacks for now
+        this.setFeedbackDefinitions({});
+    }
+}
+(0, base_1.runEntrypoint)(LinuxShowPlayerInstance, []);
+//# sourceMappingURL=main.js.map
